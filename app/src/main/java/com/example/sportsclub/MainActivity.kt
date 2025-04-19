cbaaa93b5b9d80db156ea7dc8ec9c88e2e9519ba
sanavalos <santiago.avalos@outlook.com>
2025-04-19 14:32:55 -0300

    feat: add login button on click listener

--- app/src/main/java/com/example/sportsclub/MainActivity.kt
+++ app/src/main/java/com/example/sportsclub/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.sportsclub
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,6 +17,11 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        val login = findViewById<Button>(R.id.login)
+        login.setOnClickListener {
+            val intent = Intent(this, MainMenu::class.java)
+            startActivity(intent)
         }
     }
 }
