e09c0110dabc4348d7037be5042bc0dde9e354e9
sanavalos <santiago.avalos@outlook.com>
2025-05-17 15:02:38 -0300

    feat: add actividades activity

---
+++ app/src/main/java/com/example/sportsclub/ActividadesActivity.kt
@@ -0,0 +1,27 @@
+package com.example.sportsclub
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class ActividadesActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_actividades)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        val actividadesButton = findViewById<Button>(R.id.actividadesButton)
+        actividadesButton.setOnClickListener {
+            val intent = Intent(this, MainMenu::class.java)
+            startActivity(intent)
+        }
+    }
+}
