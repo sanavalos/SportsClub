26935d7e76ea3144663addcca388d53dcefb17c9
sanavalos <santiago.avalos@outlook.com>
2025-05-18 18:59:21 -0300

    fix: add reference main menu to pago okay button

--- app/src/main/java/com/example/sportsclub/PagoOkayActivity.kt
+++ app/src/main/java/com/example/sportsclub/PagoOkayActivity.kt
@@ -1,6 +1,8 @@
 package com.example.sportsclub
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,6 +17,11 @@ class PagoOkayActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        val inicioButton = findViewById<Button>(R.id.inicioButton)
+        inicioButton.setOnClickListener {
+            val intent = Intent(this, MainMenu::class.java)
+            startActivity(intent)
         }
     }
 }
